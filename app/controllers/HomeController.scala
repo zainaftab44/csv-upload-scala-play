79c27d5a85dba2bef3f6a0681a615b78f3adf069
Zain Aftab <zainaftab44@gmail.com>
2022-08-05 16:31:30 +0500

    updated calculation code

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -11,18 +11,14 @@ import play.api.libs.json.Json
 import scala.util.parsing.json.JSONArray
 import play.api.libs.json.JsArray
 import play.api.libs.json.Writes
+import java.util.Arrays
 
 case class Shipment(date: String, country: String, weight: Long)
 
-case class CarrierRates(id: Long, country: String, weight: Long, rate: Double)
+case class CarrierRates(id: Long, country: String, weight: Long, rate: Long)
 object CarrierRates {
-//   implicit val writes: Writes[CarrierRates] = (
-//     (JsPath \ "id").write[Long] and
-//       (JsPath \ "country").write[String] and
-//       (JsPath \ "weight").write[Long] and
-//       (JsPath \ "rate").write[Double]
-//   )
   implicit val carrierWrites = Json.writes[CarrierRates]
+  implicit val empty = CarrierRates(0L, "", 0L, Long.MaxValue)
 }
 
 /** This controller creates an `Action` to handle HTTP requests to the application's home page.
@@ -43,7 +39,6 @@ class HomeController @Inject() (val controllerComponents: ControllerComponents) 
       .file("csv")
       .map { csv =>
         val filename = Paths.get(csv.filename).getFileName
-        val contentType = csv.contentType
         val path = Paths.get(s"/tmp/$filename")
 
         csv.ref.copyTo(path, replace = true)
@@ -52,25 +47,44 @@ class HomeController @Inject() (val controllerComponents: ControllerComponents) 
         val cRows = Source.fromFile(play.Environment.simple.getFile(s"/app/rates.csv")).getLines().drop(1)
 
         val carrierRates = (cRows.map(row => row.split(",")) map {
-          case Array(id, country, weight, rate) => CarrierRates(id.toLong, country, weight.toLong, rate.toDouble)
-        }).toList.sortBy(_.rate)
+          case Array(id, country, weight, rate) => CarrierRates(id.toLong, country, weight.toLong, rate.toLong)
+        }).toList.sortBy(_.weight).groupBy(_.country)
 
         val shipments = (rows.map(row => row.split(",")) map {
           case Array(date, country, weight) => Shipment(date, country, weight.toLong)
         }).toList
 
-        val rs: ListBuffer[CarrierRates] = ListBuffer();
+        val rs = ListBuffer[CarrierRates]()
 
-        shipments.foreach(s => {
-          val rates = carrierRates.filter(r => r.country.equals(s.country) && r.weight >= s.weight)
-          rs.addOne(rates.distinctBy(_.id).headOption.getOrElse(CarrierRates(0, "", 0, 0)))
-        })
-
+        shipments.foreach { s =>
+          val rates = carrierRates.get(s.country) match {
+            case Some(rate) =>
+              rate
+                .groupBy(_.id)
+                .foldLeft(CarrierRates.empty)({
+                  case (cr1, cr2) => {
+                    val r = rateForWeight(s.weight, cr2._2.collect(_.weight), cr2._2.collect(_.rate))
+                    if (cr1.rate > r) CarrierRates(cr2._1, s.country, s.weight, r) else cr1
+                  }
+                })
+            case None => throw new IllegalArgumentException()
+          }
+          rs.addOne(rates)
+        }
         Ok(Json.toJson(rs))
       }
       .getOrElse {
         Redirect(routes.HomeController.index())
           .flashing("error" -> "Missing file")
       }
+  }
+
+  val calculateShipping = (weight: Long, weights: List[Long]) => weights.indexWhere(w => weight >= w)
+
+  def rateForWeight(weight: Long, weights: List[Long], rates: List[Long]): Long = {
+    val i = calculateShipping(weight, weights)
+    if (weight <= 0 || i == -1)
+      return 0L
+    return rates(i) + rateForWeight(weight - weights(i), weights, rates)
   }
 }
---
+++ readme.txt
@@ -0,0 +1,8 @@
+Sample csv file for shipments details is test.csv. 
+
+To execute the project run 
+	> sbt compile
+Open localhost:9000 and then you can proceed with the uploading of csv and getting the results.
+ 
+ To change the data for Carriers Rates update the csv in /app/rates.csv
+ 
